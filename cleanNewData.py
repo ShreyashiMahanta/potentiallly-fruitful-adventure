f082e0afdce1a4af958463cbfb1fd6372d87d935
ShreyashiMahanta <53649054+ShreyashiMahanta@users.noreply.github.com>
2021-03-07 17:31:38 +0530

    Add files via upload

---
+++ cleanNewData.py
@@ -0,0 +1,12 @@
+import csv
+import pandas as pd
+
+df = pd.read_csv('total_stars.csv')
+
+del df['Luminosity']
+del df['Star_name']
+del df['Distance']
+del df['Mass']
+del df['Radius']
+
+df.to_csv('cleanNewData.csv')
